cb11e8ab762f1b6521d23f8a26721dd31540c904
A.Leall <83127864+oLeall99@users.noreply.github.com>
2024-10-03 22:15:53 -0300

    Exemplos de Interface Gráfica com eventos

---
+++ Teoria09exe02/src/teoria09exe02/Teoria09exe02.java
@@ -0,0 +1,11 @@
+
+package teoria09exe02;
+
+public class Teoria09exe02 {
+
+    public static void main(String[] args) {
+        Janela j = new Janela();
+        j.setVisible(true);
+    }
+    
+}
